3ea274eca97462aa8246c748811a5b0a9348eeed
nikunj-bizott <nikunj@bizott.com>
2025-03-18 11:12:34 +0530

    "fix table issue"

--- src/pages/Home/components/DataTable.tsx
+++ src/pages/Home/components/DataTable.tsx
@@ -321,6 +321,7 @@ const DataTable = () => {
                     </IconButton>
                   </Box>
                 </TableCell>
+                <TableCell></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
